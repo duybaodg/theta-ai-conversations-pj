89c0d615379542ee05323c3f4ccf2dfe590fd95c
aflahaamrin <aflaha.amrin@gmail.com>
2025-01-30 13:51:28 +1300

    Reorganise the repository

---
+++ visitor-management-agent/agentVM.py
@@ -0,0 +1,71 @@
+from __future__ import annotations
+
+import logging
+import os
+from livekit import api
+from flask import Flask
+from dotenv import load_dotenv
+
+from livekit import rtc
+from livekit.agents import (
+    AutoSubscribe,
+    JobContext,
+    WorkerOptions,
+    cli,
+    llm,
+)
+from livekit.agents.multimodal import MultimodalAgent
+from livekit.plugins import openai
+
+
+load_dotenv(dotenv_path=".env.local")
+logger = logging.getLogger("my-worker")
+logger.setLevel(logging.INFO)
+
+app = Flask(__name__)
+
+
+async def entrypoint(ctx: JobContext):
+    logger.info(f"connecting to room {ctx.room.name}")
+    await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
+
+    participant = await ctx.wait_for_participant()
+
+    run_multimodal_agent(ctx, participant)
+
+    logger.info("agent started")
+
+
+def run_multimodal_agent(ctx: JobContext, participant: rtc.RemoteParticipant):
+    logger.info("starting multimodal agent")
+
+    model = openai.realtime.RealtimeModel(
+        instructions=(
+            "You are a voice assistant created by LiveKit. Your interface with users will be voice. "
+            "You should use short and concise responses, and avoiding usage of unpronouncable punctuation. "
+            "You were created as a demo to showcase the capabilities of LiveKit's agents framework."
+        ),
+        modalities=["audio", "text"],
+        turn_detection=openai.realtime.ServerVadOptions(
+            threshold=0.95, prefix_padding_ms=200, silence_duration_ms=500
+        ),
+    )
+    agent = MultimodalAgent(model=model)
+    agent.start(ctx.room, participant)
+
+    session = model.sessions[0]
+    session.conversation.item.create(
+        llm.ChatMessage(
+            role="assistant",
+            content="Please begin the interaction with the user in a manner consistent with your instructions.",
+        )
+    )
+    session.response.create()
+
+
+if __name__ == "__main__":
+    cli.run_app(
+        WorkerOptions(
+            entrypoint_fnc=entrypoint,
+        )
+    )
