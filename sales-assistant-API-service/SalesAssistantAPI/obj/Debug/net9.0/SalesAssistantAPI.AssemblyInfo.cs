3e9fa2866d4d5870f8e5c8d2f766ffa99132b243
gitdev1 <gitdev1@gmail.com>
2025-02-10 11:05:28 +1300

    remove env

--- sales-assistant-API-service/SalesAssistantAPI/obj/Debug/net9.0/SalesAssistantAPI.AssemblyInfo.cs
+++ sales-assistant-API-service/SalesAssistantAPI/obj/Debug/net9.0/SalesAssistantAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesAssistantAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96cd0a6a99748a011794659b01a0365eb17f7b1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7287b9d56a56d829424bb43f16630f1dc9c59a66")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesAssistantAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesAssistantAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
